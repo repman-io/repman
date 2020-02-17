af669155bcc06d380d8902a879e7bab4a2a99e62
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2020-02-17 14:40:59 +0100

    Add Sentry interation for error tracking

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
