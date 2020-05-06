cd8b1ae39223d95bf457ecfd3c25db1daef90fc2
Piotr Rogowski <piotr.rogowski0@gmail.com>
2020-05-06 19:42:22 +0200

    Set GA referrer before sending pageview event (#144)

--- templates/component/js/ga.js
+++ templates/component/js/ga.js
@@ -1,11 +1,12 @@
 (function() {
     window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
     ga('create', '{{ ga_tracking }}', 'auto');
-    ga('send', 'pageview');
 
     if (isOAuthReferrer(document.referrer)) {
         ga('set', 'referrer', '{{ url("index") }}');
     }
+
+    ga('send', 'pageview');
 
     function isOAuthReferrer(referrer) {
         return referrer.indexOf('https://bitbucket.org/site/oauth2') === 0
