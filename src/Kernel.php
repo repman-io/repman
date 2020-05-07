2610199dcf649d60cf890935e66c2ffb3e381d52
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2020-05-07 13:48:33 +0200

    Release 0.2.1 (#149)

--- src/Kernel.php
+++ src/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    public const REPMAN_VERSION = '0.2.0';
+    public const REPMAN_VERSION = '0.2.1';
     private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
     public function registerBundles(): iterable
