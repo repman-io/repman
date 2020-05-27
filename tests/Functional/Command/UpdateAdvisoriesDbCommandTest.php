8b4dfc439e7f287be44f35b1279b3b9a4337b585
Piotr Rogowski <piotr.rogowski0@gmail.com>
2020-05-27 16:51:43 +0200

    Add expectations

--- tests/Functional/Command/UpdateAdvisoriesDbCommandTest.php
+++ tests/Functional/Command/UpdateAdvisoriesDbCommandTest.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Buddy\Repman\Tests\Functional\Command;
 
+use Buddy\Repman\Command\ScanAllPackagesCommand;
 use Buddy\Repman\Command\UpdateAdvisoriesDbCommand;
+use Buddy\Repman\Query\User\PackageQuery;
+use Buddy\Repman\Repository\PackageRepository;
+use Buddy\Repman\Service\Security\PackageScanner;
+use Buddy\Repman\Tests\Doubles\FakeSecurityChecker;
 use Buddy\Repman\Tests\Functional\FunctionalTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 
@@ -26,9 +31,23 @@ final class UpdateAdvisoriesDbCommandTest extends FunctionalTestCase
         $packageId = $this->fixtures->addPackage($this->buddyId, 'https://buddy.com');
         $this->fixtures->syncPackageWithData($packageId, 'buddy-works/buddy', 'Test', '1.1.1', new \DateTimeImmutable());
 
-        $commandTester = new CommandTester(
-            $this->container()->get(UpdateAdvisoriesDbCommand::class)
+        $scannerMock = $this->createMock(PackageScanner::class);
+        $scannerMock
+            ->expects(self::once())
+            ->method('scan');
+
+        $scanCommand = new ScanAllPackagesCommand(
+            $scannerMock,
+            self::$kernel->getContainer()->get('test.service_container')->get(PackageQuery::class),
+            self::$kernel->getContainer()->get('test.service_container')->get(PackageRepository::class)
         );
+
+        $command = new UpdateAdvisoriesDbCommand(
+            new FakeSecurityChecker(),
+            $scanCommand
+        );
+
+        $commandTester = new CommandTester($command);
         $result = $commandTester->execute([]);
 
         self::assertEquals($result, 0);
