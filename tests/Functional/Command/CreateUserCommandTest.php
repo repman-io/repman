57e599c5550d11ae1d62958c8d03c9153926a81e
Nicky Gerritsen <nickygerritsen@me.com>
2020-05-28 06:09:23 +0200

    Add command to create normal user (#181)

--- tests/Functional/Command/CreateUserCommandTest.php
+++ tests/Functional/Command/CreateUserCommandTest.php
@@ -4,20 +4,20 @@ declare(strict_types=1);
 
 namespace Buddy\Repman\Tests\Functional\Command;
 
-use Buddy\Repman\Command\CreateAdminCommand;
+use Buddy\Repman\Command\CreateUserCommand;
 use Buddy\Repman\Tests\Functional\FunctionalTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 
-final class CreateAdminCommandTest extends FunctionalTestCase
+final class CreateUserCommandTest extends FunctionalTestCase
 {
     public function testCreateAdmin(): void
     {
-        $commandTester = new CommandTester($this->container()->get(CreateAdminCommand::class));
+        $commandTester = new CommandTester($this->container()->get(CreateUserCommand::class));
         $commandTester->execute([
             'email' => 'test@buddy.works',
             'password' => 'password',
         ]);
 
-        self::assertStringContainsString('Created admin user with id:', $commandTester->getDisplay());
+        self::assertStringContainsString('Created user with id:', $commandTester->getDisplay());
     }
 }
