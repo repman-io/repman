be25be9bd31f3736c4a59110ea8f2ef6b62e2d52
Pedro Tanaka <pedro.tanaka@legal.one>
2020-11-25 11:04:42 +0100

    Adding connection factory for S3Client and changing .env files

---
+++ src/Service/AmazonWebServices/S3AdapterFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buddy\Repman\Service\AmazonWebServices;
+
+use Aws\S3\S3Client;
+
+final class S3AdapterFactory
+{
+    private string $region;
+
+    private bool $isOpaqueAuth;
+
+    private string $key;
+
+    private string $secret;
+
+    public function __construct(string $region, bool $isOpaqueAuth, ?string $key = null, ?string $secret = null)
+    {
+        $this->region = $region;
+        $this->isOpaqueAuth = $isOpaqueAuth;
+
+        if ($this->isOpaqueAuth) {
+            if ($key === null) {
+                throw new \InvalidArgumentException('Must pass AWS key when authentication is opaque');
+            }
+            if ($secret === null) {
+                throw new \InvalidArgumentException('Must pass AWS secret when authentication is opaque');
+            }
+
+            $this->key = $key;
+            $this->secret = $secret;
+        }
+    }
+
+    public function create(): S3Client
+    {
+        $args = [
+            'region' => $this->region,
+            'version' => 'latest',
+        ];
+
+        if ($this->isOpaqueAuth) {
+            $args['credentials'] = [
+                'key' => $this->key,
+                'secret' => $this->secret,
+            ];
+        }
+
+        return new S3Client($args);
+    }
+}
---
+++ tests/Unit/Service/AmazonWebServices/S3AdapterFactoryTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * CRM PROJECT
+ * THIS FILE IS A PART OF CRM PROJECT
+ * CRM PROJECT IS PROPERTY OF Legal One GmbH
+ *
+ * @copyright Copyright (c) 2020 Legal One GmbH (http://www.legal.one)
+ */
+
+declare(strict_types=1);
+
+namespace Buddy\Repman\Tests\Unit\Service;
+
+use Aws\Credentials\Credentials;
+use Buddy\Repman\Service\AmazonWebServices\S3AdapterFactory;
+use PHPUnit\Framework\TestCase;
+
+class S3AdapterFactoryTest extends TestCase
+{
+    public function testCreateWithMachineCredentials(): void
+    {
+        $factory = new S3AdapterFactory('eu-east-1', false, '', '');
+
+        $instance = $factory->create();
+        $cfg = $instance->getConfig();
+
+        self::assertSame('eu-east-1', $cfg['signing_region']);
+        self::assertSame('s3v4', $cfg['signature_version']);
+    }
+
+    public function testCreateWithOpaqueCredentials(): void
+    {
+        $factory = new S3AdapterFactory('eu-east-1', true, 'mykey', 'secret');
+
+        $instance = $factory->create();
+        $cfg = $instance->getConfig();
+
+        self::assertSame('eu-east-1', $cfg['signing_region']);
+        self::assertSame('s3v4', $cfg['signature_version']);
+
+        /** @var Credentials $creds */
+        $creds = $instance->getCredentials()->wait();
+        self::assertSame('mykey', $creds->getAccessKeyId());
+        self::assertSame('secret', $creds->getSecretKey());
+    }
+}
