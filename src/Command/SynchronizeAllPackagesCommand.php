282e1be3b6955990662ae2ad20938de5a4fc8a08
Sem Schilder <sem@tropical.email>
2022-01-05 10:46:46 +0100

    Add the SynchronizeAllPackages Command

---
+++ src/Command/SynchronizeAllPackagesCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buddy\Repman\Command;
+
+use Buddy\Repman\Entity\Organization;
+use Buddy\Repman\Entity\Organization\Package;
+use Buddy\Repman\Message\Organization\SynchronizePackage;
+use Buddy\Repman\Repository\OrganizationRepository;
+use Buddy\Repman\Repository\PackageRepository;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class SynchronizeAllPackagesCommand extends Command
+{
+    protected static $defaultName = 'repman:package:synchronize-all';
+
+    private MessageBusInterface $bus;
+
+    private OrganizationRepository $organizations;
+
+    public function __construct(MessageBusInterface $bus, OrganizationRepository $organizations)
+    {
+        $this->bus = $bus;
+        $this->organizations = $organizations;
+
+        parent::__construct();
+    }
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Synchronize all packages')
+            ->addArgument('organization', InputArgument::OPTIONAL, 'Organization alias')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $organizations = $this->getOrganizationsToSync($input->getArgument('organization'));
+        $output->writeln(sprintf('Synchronizing packages of %d organizations.', count($organizations)));
+
+        foreach ($organizations as $organization) {
+            $output->writeln(sprintf('Synchronizing packages for %s.', $organization->name()));
+
+            foreach ($organization->synchronizedPackages() as $package) {
+                $output->writeln(sprintf('- %s.', $package->name()));
+                $this->bus->dispatch(new SynchronizePackage($package->id()->toString()));
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param string|null $organizationAlias
+     * @return Organization[]
+     */
+    protected function getOrganizationsToSync(?string $organizationAlias): array
+    {
+        if ($organizationAlias === null) {
+            return $this->organizations->findAll();
+        }
+
+        $organization = $this->organizations->findOneBy(['alias' => $organizationAlias]);
+
+        if (!$organization instanceof Organization) {
+            throw new \InvalidArgumentException(sprintf('Organization with alias %s not found.', $organizationAlias));
+        }
+
+        return [$organization];
+    }
+}
